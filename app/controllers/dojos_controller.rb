f043a811fafa8079567963b9a3b4acf792727b1d
Felipe <felipeo510.fs@gmail.com>
2017-10-09 18:48:51 -0700

    Dojo Dashboard w/ add dojo function

--- app/controllers/dojos_controller.rb
+++ app/controllers/dojos_controller.rb
@@ -2,4 +2,24 @@ class DojosController < ApplicationController
   def index
     @dojos = Dojo.all
   end
+
+  def new
+  end
+
+  def create
+
+    dojo = Dojo.create(name:params[:name], street:params[:street], city:params[:city], state:params[:state])
+
+    if dojo.valid?
+
+      redirect_to '/'
+
+    else
+
+      flash[:error] = dojo.errors.full_messages
+      
+      redirect_to '/dojos/new'
+      
+    end
+  end
 end
